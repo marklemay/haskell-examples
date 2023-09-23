43554cf3fdab97387904790c34b54202db1c0411
Mark Lamay <lemay@bu.edu>
2023-09-23 16:46:17 -0400

    just webasm

--- 2023-07-25-overloaded-record-dot/Overloaded.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE OverloadedRecordDot #-}
-
-import           Records (Company (name, owner), Person (name), mkCompany)
-
-main = do
-  -- let c' = MkCompany "hehe" "" -- Nope
-  let c = mkCompany "DECENTRAL.EE OÜ" "Miao, ZhiCheng"
-  putStrLn $ c.name ++ " is run by " ++ c.owner.name
